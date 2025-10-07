411bff24c253dbb21b3cba4e1affea861ab5c770
moiz820 <71438635+moiz820@users.noreply.github.com>
2025-10-07 03:20:07 -0500

    Update memory_snapshot_parser.py

--- parsers/memory_snapshot_parser.py
+++ parsers/memory_snapshot_parser.py
@@ -5,12 +5,42 @@ import ast
 
 # Simulated byte offset map
 offset_map = {
-    "SADD": 0x82AFFA7,
-    "SET":  0x82AFFA7,
-    "APPEND": 0x76AEF72,
-    "HSET": 0x74A6F37,
-    "HSETNX": 0x74A6F37
+    # String commands
+    "SET":        0x82AFFA7,
+    "GET":        0x82AFAB1,
+    "APPEND":     0x76AEF72,
+    "GETDEL":     0x76AEF99,
+    "MGET":       0x76AEFBA,
+
+    # Hash commands
+    "HSET":       0x74A6F37,
+    "HSETNX":     0x74A6F37,
+    "HGET":       0x74A6F49,
+    "HGETALL":    0x74A6F62,
+    "HDEL":       0x74A6F75,
+
+    # List commands
+    "LPUSH":      0x71DFE12,
+    "RPUSH":      0x71DFE29,
+    "LPOP":       0x71DFE44,
+    "RPOP":       0x71DFE58,
+    "LSET":       0x71DFE72,
+    "LRANGE":     0x71DFE85,
+    "LREM":       0x71DFE98,
+
+    # Set commands
+    "SADD":       0x82AFFA7,
+    "SREM":       0x82AFFC0,
+    "SMEMBERS":   0x82AFFD2,
+    "SSCAN":      0x82AFFE9,
+
+    # Sorted set commands
+    "ZADD":       0x7F5AF10,
+    "ZREM":       0x7F5AF24,
+    "ZRANGE":     0x7F5AF3D,
+    "ZSCAN":      0x7F5AF52
 }
+
 
 def parse_bytes_at_offset(file_path, offset, length=512):
     with open(file_path, 'rb') as f:
